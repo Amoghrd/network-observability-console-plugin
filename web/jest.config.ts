70e9de16ba45054481b1b2f22ff24cc7583c6eb7
Amoghrd <aramesha@redhat.com>
2026-09-09 10:20:52 -0400

    Remove Jest configuration changes
    Per review feedback from jpinsonneau, the Jest test fixes are not needed
    as tests pass fine on main branch. Reverting jest.config.ts and
    setup-tests.tsx to their main branch versions.
    
    Changes removed:
    - Custom resolver (ts-jest-resolver)
    - PatternFly react-charts/victory mock
    - Formatting changes
    
    Related: https://github.com/netobserv/netobserv-web-console/pull/1723

--- web/jest.config.ts
+++ web/jest.config.ts
@@ -3,25 +3,40 @@ import type { Config } from 'jest';
 const config: Config = {
   verbose: true,
   collectCoverage: true,
-  coverageReporters: ['cobertura'],
+  coverageReporters: [
+    "cobertura"
+  ],
   maxWorkers: 1,
-  globals: {},
-  setupFiles: ['<rootDir>/jest.setup.ts'],
-  setupFilesAfterEnv: ['<rootDir>setup-tests.tsx'],
-  preset: 'ts-jest',
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  modulePathIgnorePatterns: ['cypress'],
+  globals: {
+  },
+  setupFiles: [
+    "<rootDir>/jest.setup.ts"
+  ],
+  setupFilesAfterEnv: [
+    "<rootDir>setup-tests.tsx"
+  ],
+  preset: "ts-jest",
+  moduleFileExtensions: [
+    "js",
+    "jsx",
+    "ts",
+    "tsx"
+  ],
+  modulePathIgnorePatterns: [
+    "cypress"
+  ],
   moduleNameMapper: {
-    '\\.(s?css|less)$': '<rootDir>/pluginToStandaloneMapper/mapper.tsx',
-    '^@console/.*$': '<rootDir>/pluginToStandaloneMapper/mapper.tsx'
+    "\\.(s?css|less)$": "<rootDir>/pluginToStandaloneMapper/mapper.tsx",
+    "^@console/.*$": "<rootDir>/pluginToStandaloneMapper/mapper.tsx"
   },
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.[t|j]sx?$': 'ts-jest'
+    "^.+\\.[t|j]sx?$": "ts-jest"
   },
   transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!(@patternfly|@openshift-console|@spice-project|d3.*|internmap|delaunator|robust-predicates\\S*?)/.*)'
-  ]
+    "<rootDir>/node_modules/(?!(@patternfly|@openshift-console|@spice-project|d3.*|internmap|delaunator|robust-predicates\\S*?)/.*)"
+  ],
+  resolver: "ts-jest-resolver"
 };
 
 export default config;
